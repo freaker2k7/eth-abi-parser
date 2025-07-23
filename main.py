9493d09ae93fb1fdadcbcee459fd91d4b27d85e8
Evgeny Kolyakov <freaker2k7@users.noreply.github.com>
2025-07-23 19:21:43 +0300

    Added take_function() test

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
         'event TestEvent(uint indexed id, (string, uint16, (uint8, uint8)) value)')
     print(parser.take_event())
 
+    parser = HumanReadableParser(
+        'function doTransferOut(address to, uint amount) external returns (bool success)')
+    print(parser.take_function())
+
 
 if __name__ == '__main__':
     main()
